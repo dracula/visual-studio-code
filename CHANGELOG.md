3156a2890b03f265ef8d0637d4191da5bea843f8
Derek Sifford <dereksifford@gmail.com>
2017-02-27 20:16:42 -0500

    Development refactor + v1.3.0 (#8)
    * Refactor to make maintenance life easier
    
    * remove test file
    
    * adjust filenames
    
    * final updates prior to 1.3.0

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+# 1.3.0
+
+- Change Maintainers
+- Add Markdown Syntax.
+- Add better support for TypeScript.
+- Add base language fallbacks to better support esoteric languages.
+
+Please feel free to request changes or leave feedback.
---
+++ build.js
@@ -0,0 +1,28 @@
+const yaml = require('js-yaml');
+const fs = require('fs');
+
+const doc = yaml.safeLoad(fs.readFileSync(__dirname + '/src/dracula.yml', 'utf8'));
+const { scheme } = doc;
+
+fs.writeFileSync(__dirname + '/theme/dracula.json', JSON.stringify(parseColors(doc), null, 4));
+
+function parseColors(input) {
+    if (Array.isArray(input)) {
+        return input.map(i => {
+            if (typeof i === 'object' && i !== null) {
+                return parseColors(i);
+            }
+            return i;
+        });
+    }
+    return Object.keys(input).reduce((prev, curr) => {
+        if (typeof input[curr] === 'object' && input[curr] !== null) {
+            return Object.assign({}, prev, { [curr]: parseColors(input[curr]) });
+        }
+        return Object.assign({}, prev, {
+            [curr]: Object.keys(scheme).indexOf(input[curr]) > -1
+                ? scheme[input[curr]]
+                : input[curr],
+        });
+    }, {});
+}
