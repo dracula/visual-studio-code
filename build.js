1ecac1e2fca01617cc9884bca434115b0456db52
Derek P Sifford <dereksifford@gmail.com>
2018-08-15 12:00:26 -0400

    update build file so it works on windows machines
    closes #83

--- build.js
+++ build.js
@@ -1,18 +1,26 @@
 const fs = require('fs');
+const path = require('path');
 const tinycolor = require('tinycolor2');
 const yaml = require('js-yaml');
 
+const THEME_DIR = path.join(__dirname, 'theme');
+const THEME_YAML_FILE = path.join(__dirname, 'src', 'dracula.yml');
+
+if (!fs.existsSync(THEME_DIR)) {
+    fs.mkdirSync(THEME_DIR);
+}
+
 const withAlphaType = new yaml.Type('!alpha', {
     kind: 'sequence',
-    construct: (data) => {
+    construct: data => {
         return data[0] + data[1];
     },
-    represent: (data) => {
-        return [ data[0], data[1] ];
-    }
+    represent: data => {
+        return [data[0], data[1]];
+    },
 });
-const schema = yaml.Schema.create([ withAlphaType ]);
-const standard = fs.readFileSync(`${__dirname}/src/dracula.yml`, 'utf8');
+const schema = yaml.Schema.create([withAlphaType]);
+const standard = fs.readFileSync(THEME_YAML_FILE, 'utf8');
 
 yamlObj = yaml.load(standard, { schema });
 
@@ -23,14 +31,22 @@ yamlObj.colors = Object.keys(yamlObj.colors).reduce((obj, key) => {
     return Object.assign({}, obj, { [key]: yamlObj.colors[key] });
 }, {});
 
-const brightColors = [ ...yamlObj.dracula.ansi, ...yamlObj.dracula.brightOther ];
+const brightColors = [...yamlObj.dracula.ansi, ...yamlObj.dracula.brightOther];
 
 const soft = standard.replace(/'(#[0-9A-Z]{6})/g, (match, hex) => {
     if (brightColors.indexOf(hex) > -1) {
-        return `'${tinycolor(hex).desaturate(20).toHexString()}`;
+        return `'${tinycolor(hex)
+            .desaturate(20)
+            .toHexString()}`;
     }
     return `'${tinycolor(hex).toHexString()}`;
 });
 
-fs.writeFileSync(`${__dirname}/theme/dracula.json`, JSON.stringify(yaml.load(standard, { schema }), null, 4));
-fs.writeFileSync(`${__dirname}/theme/dracula-soft.json`, JSON.stringify(yaml.load(soft, { schema }), null, 4));
+fs.writeFileSync(
+    path.join(THEME_DIR, 'dracula.json'),
+    JSON.stringify(yaml.load(standard, { schema }), null, 4)
+);
+fs.writeFileSync(
+    path.join(THEME_DIR, 'dracula-soft.json'),
+    JSON.stringify(yaml.load(soft, { schema }), null, 4)
+);
