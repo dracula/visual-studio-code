1cd5d4082a793a8a848570ecab14a4f8acaea3f2
Daniel Ramos <danielramosacosta@hotmail.com>
2018-10-11 09:58:50 +0100

    Rename function and format code

--- lint.js
+++ lint.js
@@ -1,41 +1,43 @@
-"use strict";
+'use strict';
 
-const https = require("https");
-const fs = require("fs");
-const util = require("util");
+const https = require('https');
+const fs = require('fs');
+const util = require('util');
 
 const get = url =>
     new Promise((resolve, reject) => {
         https.get(url, res => {
-            let body = "";
-            res.setEncoding("utf8");
-            res.on("data", data => (body += data));
-            res.on("end", () => resolve(body));
-            res.on("error", reject);
+            let body = '';
+            res.setEncoding('utf8');
+            res.on('data', data => (body += data));
+            res.on('end', () => resolve(body));
+            res.on('error', reject);
         });
     });
 
 const readFile = util.promisify(fs.readFile);
 
 const THEME_COLOR_REFERENCE_URL =
-    "https://code.visualstudio.com/docs/getstarted/theme-color-reference";
+    'https://code.visualstudio.com/docs/getstarted/theme-color-reference';
 
 const NOT_THEME_KEYS = [
-    "workbench.colorCustomizations",
-    "editor.tokenColorCustomizations"
+    'workbench.colorCustomizations',
+    'editor.tokenColorCustomizations',
 ];
 
-async function scrapThemeAvailableKeys() {
+async function scrapeThemeAvailableKeys() {
     const data = await get(THEME_COLOR_REFERENCE_URL);
 
-    const matches = data.match(new RegExp("<code>.+?</code>", "g"));
+    const matches = data.match(new RegExp('<code>.+?</code>', 'g'));
 
     if (!matches) {
-        throw new Error("Couldn't find any matches with <code>...</code>, maybe docs have chaged?")
+        throw new Error(
+            "Couldn't find any matches with <code>...</code>, maybe docs have chaged?"
+        );
     }
 
     const availableKeys = [...matches]
-        .map(key => key.replace("<code>", "").replace("</code>", ""))
+        .map(key => key.replace('<code>', '').replace('</code>', ''))
         .filter(key => !/ /.test(key)) // Remove if contains spaces
         .filter(key => !/#.../.test(key)) // Remove if is a hex color
         .filter(key => !/&quot;/.test(key)) // Remove if contains quotes
@@ -46,7 +48,7 @@ async function scrapThemeAvailableKeys() {
 }
 
 async function getTheme(fileToLint) {
-    const fileContent = await readFile(fileToLint, "utf8");
+    const fileContent = await readFile(fileToLint, 'utf8');
     const theme = JSON.parse(fileContent);
     return theme;
 }
@@ -55,12 +57,12 @@ async function getTheme(fileToLint) {
     const fileToLint = process.argv[2];
 
     if (!fileToLint) {
-        throw new Error("You need to specify the path of the theme to lint");
+        throw new Error('You need to specify the path of the theme to lint');
     }
 
     const [availableKeys, theme] = await Promise.all([
-        scrapThemeAvailableKeys(),
-        getTheme(fileToLint)
+        scrapeThemeAvailableKeys(),
+        getTheme(fileToLint),
     ]);
 
     Object.keys(theme.colors).forEach(key => {
