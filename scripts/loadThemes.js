bc02eacb97014588d3239c3a5497f8b3093e3d6a
Daniel Ramos <danielramosacosta@hotmail.com>
2019-02-11 23:38:49 +0000

    Add debugging profile for development

--- scripts/loadThemes.js
+++ scripts/loadThemes.js
@@ -4,8 +4,20 @@ const tinycolor = require('tinycolor2');
 const fsp = require('./fsp');
 const { loadYAML } = require('./yaml');
 
-async function loadTheme(yamlFilePath) {
+/**
+ * On dev mode, sometimes when we read the file the return value of readFile
+ * is an empty string. In those cases we need to read the file again
+ */
+async function readFileRetrying(yamlFilePath) {
     const standardThemeYAML = await fsp.readFile(yamlFilePath, 'utf8');
+    if (!standardThemeYAML) {
+        return readFileRetrying(yamlFilePath);
+    }
+    return standardThemeYAML;
+}
+
+async function loadTheme(yamlFilePath) {
+    const standardThemeYAML = await readFileRetrying(yamlFilePath);
     const standardTheme = await loadYAML(standardThemeYAML);
 
     const softThemeYAML = getSoftThemeYAML(standardThemeYAML, standardTheme);
